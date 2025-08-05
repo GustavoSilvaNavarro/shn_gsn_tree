873b6f33999e5da614ffa4ad7a6cbedfae00c4fc
Gustavo Silva <gustavo.silva.nav@gmail.com>
2025-08-05 12:16:12 -0300

    ♻️ Modify the type so it aligns to code style

--- src/interfaces/node.interfaces.ts
+++ src/interfaces/node.interfaces.ts
@@ -5,5 +5,5 @@ export type SingleNode = { id: number; label: string; parent_id: Nullable<number
 export type TreeNodeResponse = {
   id: number;
   label: string;
-  children: TreeNodeResponse[];
+  children: Array<TreeNodeResponse>;
 };
